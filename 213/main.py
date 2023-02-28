ff57127e14ade948772ea7f5e66eeef44922eca2
student <masha.shamburova@mail.ru>
2023-02-28 16:15:09 +0300

    lab2

---
+++ 213/main.py
@@ -0,0 +1,57 @@
+# a=input("Придумайте пароль:")
+# if len(a)<5:
+#     print("Пароль слишком короткий")
+# elif a[0:6]== "qwerty":
+#     print("Пароль ненадежный")
+# else:
+#     print("Пароль подходит")
+
+
+
+
+# number = int(input("введите номер места "))
+# if number % 2 == 0 and number <36:
+#     print("Верхнее место в купе")
+#
+# elif number %2!=0 and number <36:
+#     print("Нижнее место в купе")
+#
+# else:
+#     if range(37, 54):
+#         print("Бокове место")
+
+
+
+
+
+# year=int(input("Введите год:"))
+# if year%4==0 and year%400==0:
+#     print("Год",year," високосный")
+# else:
+#  print("Год",year,"невисокосный")
+
+
+
+
+#color=('красный ','синий','желтый')
+#c1=input()
+#c2=input()
+#if c1 in color and c2 in color:
+#    if c1!=c2 :
+#     if (c1 in ('красный', 'синий')) and (c2 in ('красный', 'синий')):
+#        print("фиолетовый")
+#     if (c1 in ('красный', 'желтый')) and (c2 in ('красный', 'желтый')):
+#        print('оранжевый')
+#     if (c1 in ('синий', 'желтый')) and (c2 in ('синий', 'желтый')):
+#         print('зеленый')
+#    else:
+#        print(c1)
+#else:
+# print('Ошибка цвета')
+
+
+# n=int(input("Введите количество слов: "))
+# w=[]
+# for i in range(n):
+#     w.append(str(input()))
+# print(" ".join(w))
